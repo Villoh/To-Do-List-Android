f44b4b4d5926b75a10297dd8ce36604e374d6cbf
Mikel Villota <mvillotag01@educantabria.es>
2023-02-24 18:05:00 +0100

    Actividad inicio realizada, diseño main activity finalizado.

---
+++ app/src/main/java/com/mikel/todolist/InicioActivity.java
@@ -0,0 +1,40 @@
+package com.mikel.todolist;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class InicioActivity extends AppCompatActivity {
+
+    private ImageView logoApp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        //Se oculta la action bar superior
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.hide();
+        }
+        //Animación de aparición para la imagen
+        logoApp = findViewById(R.id.imageViewLogo);
+        Animation animacionAparecer = AnimationUtils.loadAnimation(getBaseContext(), R.anim.aparecer);
+        logoApp.setAnimation(animacionAparecer);
+        //Se inicia la actividad inicio y pasados los 4 segundos se muestra la actividad principal.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(InicioActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
